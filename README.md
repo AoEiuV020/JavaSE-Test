8a4455cfd961596bd2e1d79fa2e10d75d5ff25d2
AoEiuV020 <aoeiuv020@gmail.com>
2017-04-29 17:45:36 +0800

    Init java-library

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JavaTestUseJunit
 java一些api之类的不熟悉的在这里用junit测试下，
+
+```sh
+./gradlew test --tests aoeiuv020.HelloTest.hello
+```
---
+++ src/test/java/aoeiuv020/HelloTest.java
@@ -0,0 +1,15 @@
+package aoeiuv020;
+import java.util.*;
+import java.io.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Created by AoEiuV020 on 2017/04/29.
+ */
+
+public class HelloTest{
+    @Test
+    public void hello() {
+        assertEquals("HelloTest",HelloTest.class.getSimpleName());
+    }
+}
